223a336bb2e9d441e4519020f337dbdd7df9d71a
Travelling Fern ApS <tf@travellings-mbp.home>
2020-03-01 23:23:37 +0100

    test commit

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { GeoDataManager } from "./GeoDataManager";
 import { GeoDataManagerConfiguration } from "./GeoDataManagerConfiguration";
 import { GeoTableUtil } from "./util/GeoTableUtil";
 
+// Just testing 
+
 export {
   GeoDataManager,
   GeoDataManagerConfiguration,
